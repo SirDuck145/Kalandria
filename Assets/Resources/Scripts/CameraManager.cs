ac77a0447f6e2a59879fb8f89bb17de3d42620f4
SirDuck145 <matthiasharden@gmail.com>
2020-03-10 20:39:38 -0700

    Add the basic network transitions into a dev mode

---
+++ Assets/Resources/Scripts/CameraManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+
+namespace Com.LunacyIncorporation.Kalandria
+{
+    public class CameraManager : MonoBehaviourPun
+    {
+
+        #region Public Fields
+
+        [Tooltip("The distance the mouse must be from the edge of the screen to scroll")]
+        [SerializeField]
+        public int screenBounds;
+
+        [Tooltip("The speed at which the camera pans")]
+        [SerializeField]
+        public int panSpeed;
+
+
+        #endregion
+
+        #region Private Fields
+
+        private int screenWidth;
+        private int screenHeight;
+
+        #endregion
+
+
+        void Start()    
+        {
+            screenWidth = Screen.width;
+            screenHeight = Screen.height;
+        }
+
+        private void Update()
+        {
+            ProcessInputs();
+        }
+
+        private void LateUpdate()
+        {
+            // Move the camera position the be the position of the local cameraManager
+            Camera.main.transform.position = gameObject.transform.position;
+        }
+
+        /// <summary>
+        /// Handles the inputs from the user, moving the camera around the screen accordingly
+        /// </summary>
+        void ProcessInputs()
+        {
+            // If it is not your camera, exit
+            
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // Check if the mouse is within the bounds 
+            Debug.Log("Processing the inputs");
+            Debug.Log(Input.mousePosition.x);
+            Debug.Log(screenWidth / 2 - 50);
+
+            // Mouse is near the right side
+            if (Input.mousePosition.x > screenWidth / 2 - screenBounds)
+            {
+                Debug.Log("Mouse is near the right side of the screen");
+                gameObject.transform.position = new Vector3(panSpeed*Time.deltaTime, 0, 0);
+            }
+        }
+    }
+
+}
---
+++ Assets/Resources/Scripts/LobbyManager.cs
@@ -0,0 +1,143 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Com.LunacyIncorporation.Kalandria
+{
+    public class LobbyManager : MonoBehaviourPunCallbacks
+    {
+
+        #region Public Fields
+
+
+        [Tooltip("The prefab used to instance the player")]
+        public GameObject playerPrefab;
+
+
+        #endregion
+
+
+        #region Private Serializable Fields
+
+
+        [Tooltip("Maximum number of players")]
+        private byte maxPlayers = 2;
+
+
+        #endregion
+
+        #region Private Fields
+
+        private string gameVersion = "0.1";
+
+        private bool isConnecting;
+
+
+        #endregion
+
+        #region MonoBehavior Callbacks
+
+
+        private void Awake()
+        {
+            // *Critical*
+            // this makes sure we can use PhotonNetwork.loadLevel() on the master client and all clients in that same room sync their level
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Start the connection process.
+        /// - if already connected, attemp to join a random room
+        /// - if not yet connected, connect this application instance to photon cloud network
+        /// </summary>
+        public void Connect()
+        {
+            Debug.Log("Connect fires");
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+
+            if (PhotonNetwork.IsConnected)
+            {
+                // *Critical*, Attempt joining a random room
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+
+        #endregion
+
+
+        #region MonoBehaviorPunCallbacks Callbacks
+
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Pun basics tut/launcher: OnConnectedToMaster() was called by PUN");
+            // #Critical: We try to join a random room, if we can't we will be called back with OnJoinRandomFailed()
+            if (isConnecting)
+            {
+                PhotonNetwork.JoinRandomRoom();
+                isConnecting = false;
+            }
+        }
+
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            //progressLabel.SetActive(false);
+            //controlPanel.SetActive(true);
+            Debug.LogWarningFormat("PUN Basics Tut/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            // #Critical: we failed to join a random room, maybe none exist or are full.
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayers });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+            //#Critical: we only load if we are the first player, else we rely on the auto sync
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("We load the room for one player");
+
+                // #Critical
+                // Load the Room Level --> This will be a lobby soon
+                PhotonNetwork.LoadLevel("LobbyRoom");
+            }
+            else
+            {
+                if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+                {
+                    Debug.Log("Load the full game room");
+                    PhotonNetwork.LoadLevel("GameMapTest");
+                }
+            }
+        }
+
+        public void DevLoad()
+        {
+            Debug.Log("Load the test room");
+            PhotonNetwork.LoadLevel("GameMapTest");
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+        #endregion
+
+    }
+}
+
